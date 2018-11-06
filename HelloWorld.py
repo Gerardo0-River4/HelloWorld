4c77cce1a7918387e9229229379fc6e4cdd7f2ff
Gerardo0-River4 <44813930+Gerardo0-River4@users.noreply.github.com>
2018-11-06 10:09:47 -0700

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,9 @@
+print("Are you a micheals rewards member?")
+var = 2018
+print("Today is monday, November 5" , var)
+print(7+ 8)
+print(7 * 8)
+print(5**2)
+print("My name is Gerardo Rivera, i like coding")
+print("My name is Gerardo Rivera" + ", I like coding")
+print("My name is Gerardo Rivera, I like coding\n"*5)
